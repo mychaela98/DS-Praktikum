aed8735ca5f88b5c93c848de3eb4526449dffb18
mychaela98 <mychaela.k98@gmail.com>
2020-10-19 10:22:01 +0200

    added Assignment_02 with data

---
+++ Assignment_02/Assignment_02.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.layers.experimental import preprocessing
+
+
